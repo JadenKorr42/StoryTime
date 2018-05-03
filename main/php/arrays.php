b789265b4a3e027c9f01692ccfad321ec4e77a0b
JadenKorr42 <36278915+JadenKorr42@users.noreply.github.com>
2018-05-03 13:56:11 -0400

    New Requirements

--- main/php/arrays.php
+++
@@ -1,6 +0,0 @@
-<?php
-$title=array("Puppies", "Kittens", "Dogs", "Cats");
-
-$author=array("Johnathon Doyle", "Doyle Jacobs", "Steven Johnson", "Donavon Spielsburg");
-
-?>
--- main/php/entries.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Load Arrays
-require 'arrays.php';
-
-array_push($title, story_title);
-array_push($author, story_author);
-
-?>
